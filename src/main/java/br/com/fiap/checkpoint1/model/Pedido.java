aff8f7f34bc7031cffa94968f7645fa0fd69e339
GabrielCatanzaro <gabrielgomescatanzaro@gmail.com>
2025-03-26 18:50:38 -0300

    Começo do projeto instalação do maven

---
+++ src/main/java/br/com/fiap/checkpoint1/model/Pedido.java
@@ -0,0 +1,23 @@
+package br.com.fiap.checkpoint1.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDate;
+
+@Data
+@Entity
+public class Pedido {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String clienteNome;
+
+    @Column(nullable = false)
+    private LocalDate dataPedido = LocalDate.now();
+
+    @Column(nullable = false)
+    private double valorTotal;
+}
